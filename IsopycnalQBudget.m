7b42c8f3c271c3ef784c23371496d1b1fb4230bf
Jacob Wenegrat <jacob13@cees-mazama.stanford.edu>
2016-06-08 17:08:19 -0700

    Updating Iso and CalcQ to return all the terms, and calculate PV using the direct formulation.

--- IsopycnalQBudget.m
+++ IsopycnalQBudget.m
@@ -7,7 +7,7 @@ dx = 1000; dy = dx; dz = 2.5;
 nx = 96; ny=nx; nz=200;
 ts = 1800;
 TtoB = 9.81.*2e-4;
-tslice = [4000 7000];
+tslice = [1 1339];
 slice={0, 0, 0, tslice};%100 120
 sliceEta={0,0,[1 1],tslice};%251 271
 
@@ -16,6 +16,7 @@ metric = permute(repmat(ztmp, [1, nx, ny, 1]), [2 3 1 4]);
 %%
 % CALCULATE TERMS
 Q = NaN(nx, ny, nz, tslice(end)-tslice(1)+1);
+Qdir = Q;
 JBx = Q;
 JBy = Q;
 JBz = Q;
@@ -29,28 +30,29 @@ mask = Q;
 FricDiv = Q;
 AdvDiv = Q;
 DiaDiv = Q;
+THETA  = NaN(nx, ny, nz, tslice(end)-tslice(1)+1);
+
 for i=1:(tslice(end)-tslice(1)+1)
     disp(num2str(i))
    slicetemp = {slice{1}, slice{2}, slice{3}, [tslice(1)+i-1 tslice(1)+i-1]};
-   [Q(:,:,:,i), JAx(:,:,:,i), JAy(:,:,:,i), JAz(:,:,:,i), ...
+   [Q(:,:,:,i), Qdir(:,:,:,i), JAx(:,:,:,i), JAy(:,:,:,i), JAz(:,:,:,i), ...
        JFx(:,:,:,i), JFy(:,:,:,i), JFz(:,:,:,i), JBx(:,:,:,i), JBy(:,:,:,i), JBz(:,:,:,i)] ...
-       = calcQBudget(diagfile, statefile, etanfile, [nx, ny], slicetemp);
+       = calcQBudget(diagfile, statefile, etanfile, [nx, ny], slicetemp, dx, dy);
 
    
    FricDiv(:,:,:,i) = Drv(dx, JFx(:,:,:,i), 'x') + Drv(dy, JFy(:,:,:,i), 'y') + Drv(metric, JFz(:,:,:,i), 'z');
    AdvDiv(:,:,:,i) = Drv(dx, JAx(:,:,:,i), 'x') + Drv(dy, JAy(:,:,:,i), 'y') + Drv(metric, JAz(:,:,:,i), 'z');
    DiaDiv(:,:,:,i) = Drv(dx, JBx(:,:,:,i), 'x') + Drv(dy, JBy(:,:,:,i), 'y') + Drv(metric, JBz(:,:,:,i), 'z');
+   THETA(:,:,:,i) = GetVar(statefile, diagfile, {'THETA', '(1)'}, slicetemp);
 
 end
 
 %%
 % Generate Mask
-isoT = [16.75 17];
-
-THETA  = NaN(nx, ny, nz, tslice(end)-tslice(1)+1);
+isoT = [16.5 16.55];
+% isoT = [0 20];
 for i=1:(tslice(end)-tslice(1)+1);
-   slicetemp = {slice{1}, slice{2}, slice{3}, [tslice(1)+i-1 tslice(1)+i-1]};
-     THETA(:,:,:,i) = GetVar(statefile, diagfile, {'THETA', '(1)'}, slicetemp);
+%    slicetemp = {slice{1}, slice{2}, slice{3}, [tslice(1)+i-1 tslice(1)+i-1]};
    mask(:,:,:,i) = (THETA(:,:,:,i)>isoT(1)) & (THETA(:,:,:,i)<isoT(2));
 end
 %%
@@ -62,7 +64,7 @@ disp('Integrate Over Volume');
 ylims = 2:(ny-2);
 zlims = 2:nz-1;
 
-Qi = Q.*mask;
+
 % JAxi = JAx.*mask; JAyi = JAy.*mask; JAzi = JAz.*mask;
 % JFxi = JFx.*mask; JFyi = JFy.*mask; JFzi = JFz.*mask;
 % JBxi = JBx.*mask; JByi = JBy.*mask; JBzi = JBz.*mask;
@@ -70,17 +72,39 @@ Frici = FricDiv.*mask;
 Advi = AdvDiv.*mask;
 Diai = DiaDiv.*mask;
 
-vol = squeeze(sum(sum(sum(mask))));
-
-Fric = squeeze(nansum(nansum(nansum(Frici))))./vol;
+vol = dx.*dy.*dz.*squeeze(sum(sum(sum(mask))));
+gridvol = dx.*dy.*dz;
+Fric = squeeze(nansum(nansum(nansum(Frici)))).*gridvol./vol;
 Frict = cumtrapz(Fric).*ts;
-Adv = squeeze(nansum(nansum(nansum(Advi))))./vol;
+Adv = squeeze(nansum(nansum(nansum(Advi)))).*gridvol./vol;
 Advt = cumtrapz(Adv).*ts;
-Dia = squeeze(nansum(nansum(nansum(Diai))))./vol;
+Dia = squeeze(nansum(nansum(nansum(Diai)))).*gridvol./vol;
 Diat = cumtrapz(Dia).*ts;
-Qa = squeeze(nansum(nansum(nansum(Qi))))./vol;
-Qa = Qa - Qa(1);
-% 
+
+zl  = 1;
+yl = 2;
+Frics = squeeze(nansum(nansum(JFz(:,:,zl,:).*mask(:,:,zl,:))) - nansum(nansum(JFz(:,:,end-zl+1,:).*mask(:,:,end-zl+1,:)))).*dx.*dy;
+Frics = Frics+squeeze(nansum(nansum(JFz(:,end-yl,:,:).*mask(:,end-yl,:,:))) - nansum(nansum(JFz(:,yl,:,:).*mask(:,yl,:,:)))).*dz.*dx;
+Frics = Frics./vol;
+Fricst = cumtrapz(Frics).*ts;
+% Dias = squeeze(nansum(nansum(JBz(:,:,zl,:).*mask(:,:,zl,:))));
+Dias = squeeze(nansum(nansum(JBz(:,:,zl,:).*mask(:,:,zl,:))) - nansum(nansum(JBz(:,:,end-zl+1,:).*mask(:,:,end-zl+1,:)))).*dx.*dy;
+Dias = Dias+squeeze(nansum(nansum(JBz(:,end-yl,:,:).*mask(:,end-yl,:,:))) - nansum(nansum(JBz(:,yl,:,:).*mask(:,yl,:,:)))).*dz.*dx;
+Dias = Dias./vol;
+Diast = cumtrapz(Dias).*ts;
+
+Qi = Qdir.*mask;
+Qta = squeeze(nansum(nansum(nansum(Qi)))).*gridvol./vol; %This is volume integral of dQ/dt
+Qda = cumtrapz(Qta).*ts; %Need to check if this is correct.
+Qda = Qda - Qda(1);
+
+Qi = Q.*mask;
+% Qt = gradient
+Qa = squeeze(nansum(nansum(nansum(Qi)))).*gridvol./vol; %Note that doing it this way is incorrect, as it doesn't account for time variations in Vol...
+
+% Qda = cumtrapz(Qta).*ts; %Need to check if this is correct.
+Qt = gradient(Qa, ts);
+Qa = Qa - Qa(1);% 
 % Jbah = squeeze(nansum(nansum(JBz(:,ylims,:,:))))*dx*dy./area;
 % Jfah = squeeze(nansum(nansum(JFz(:,ylims,:,:))))*dx*dy./area;
 % qah = squeeze(nansum(nansum(Q(:,ylims,:,:)))).*dx.*dy./area;
@@ -118,19 +142,41 @@ Qa = Qa - Qa(1);
 % qa = -nansum(qah)*dz; %integrate in z
 % qa = cumtrapz(qa)*ts; %integrate in time.
 
-% qdira = -nansum(qdirah)*dz; %integrate in z
+% qdira = -nansum(qdirah)*dz; %integrate in 
 % qdira = cumtrapz(qdira)*ts; %integrate in time.
-
+%%
 % Make Time Series Figure of DeltaQ and Fluxes
 figure
 plot(Qa, 'LineWidth', 2)
 hold on
 plot(-Frict, 'LineWidth', 2);
+
 plot(-Advt, 'LineWidth', 2);
 plot(-Diat, 'LineWidth', 2);
 
-plot(-(Frict+Advt+Diat), 'LineWidth', 2, 'LineStyle', '--');
+plot(-(Frict+Diat), 'LineWidth', 3, 'LineStyle', '--');
 % plot(qdira);
+plot(Qda, 'LineWidth', 2)
+plot(-Fricst); 
+plot(-Diast);
+plot(-(Fricst+Diast));
+hold off
+legend('Q', 'Fric','Adv', 'Dia', 'Sum', 'Qdir', 'Fric(0)', 'Dia(0)', 'Sum(0)');
+xlabel('Num time steps (Hourly)');
+ylabel('\Delta Q');
+grid on
+
+%%
+figure
+plot(Qt, 'LineWidth', 2)
+hold on
+plot(-Fric, 'LineWidth', 2);
+plot(-Adv, 'LineWidth', 2);
+plot(-Dia, 'LineWidth', 2);
+
+plot(-(Fric+Adv+Dia), 'LineWidth', 2, 'LineStyle', '--');
+% plot(qdira);
+plot(Qta, 'LineWidth', 2)
 
 hold off
 legend('Q', 'Fric','Adv', 'Dia', 'Sum');
@@ -138,3 +184,7 @@ xlabel('Num time steps (Hourly)');
 ylabel('\Delta Q');
 grid on
 
+
+% %%
+% plot(Qa);
+% plot(
--- calcQBudget.m
+++ calcQBudget.m
@@ -1,4 +1,4 @@
-function [Q, JADVx, JADVy, JADVz, JFx, JFy, JFz, JBx, JBy, JBz] = calcQBudget(diagfile, statefile, etanfile, sizes, slice )
+function [Q,Qdir, JADVx, JADVy, JADVz, JFx, JFy, JFz, JBx, JBy, JBz] = calcQBudget(diagfile, statefile, etanfile, sizes, slice, dx, dy )
 nx = sizes(1); ny = sizes(2);
 sliceEta = {slice{1}, slice{2}, [1 1], slice{4}};
 TtoB = 9.81.*2e-4;
@@ -42,16 +42,28 @@ bz = GetVar(statefile, diagfile, {'b', 'Dz(1)'}, slice);
 %ignore w derivatives for consistency with hydrostatic approx.
 OMEGAX = GetVar(statefile, diagfile, {'WVEL', 'VVEL', 'Dy(1) - Dz(2)'}, slice);
 OMEGAY = GetVar(statefile, diagfile, {'UVEL', 'WVEL', 'Dz(1) - Dx(2)'}, slice);
-
-% OMEGAX = GetVar(statefile, diagfile, { 'VVEL', ' - Dz(1)'}, slice);
-% OMEGAY = GetVar(statefile, diagfile, {'UVEL', 'Dz(1) '}, slice);
 OMEGAZ = GetVar(statefile, diagfile, {'f_1e-4', 'VVEL', 'UVEL', '(1) + Dx(2) - Dy(3)'}, slice);
-% if ndims(squeeze(OMEGAZ))==3; 
-%     OMEGAZ = GetVar(statefile, diagfile, {'f_1e-4', 'UVEL', '(1)  - Dy(2)'}, slice);
-%     OMEGAY = Drv(metric, GetVar(statefile, diagfile, {'UVEL', '(1)'}, slice),'z');
-% end
-%%
 Q = OMEGAX.*bx + OMEGAY.*by + OMEGAZ.*bz; %A more direct definition of Q.
+
+%%
+%Calculate Q from the momentum budget:
+LHSV = GetVar(statefile, diagfile, {'TOTVTEND', 'Vm_Advec', '(1)/86400-(2)'}, slice);
+LHSV = LHSV-dpdy;
+LHSU = GetVar(statefile, diagfile, {'TOTUTEND', 'Um_Advec', '(1)/86400-(2)'}, slice);
+LHSU = LHSU-dpdx;
+QXz = Drv(metric, LHSU, 'z',1,1);
+QXy = Drv(dy, LHSU, 'y', 1,1);
+QVz = Drv(metric, LHSV, 'z', 1, 1);
+QVx = Drv(dx, LHSV, 'x',1,1);
+Qmom = -bx.*QVz + by.*QXz + bz.*(QVx - QXy);
+
+LHSb = TtoB.*GetVar(statefile, diagfile, {'TOTTTEND', 'UDIAG1', '(1)/86400-(2)'}, slice);
+LHBx = Drv(dx, LHSb, 'x', 1, 1);
+LHBy = Drv(dy, LHSb, 'y', 1, 1);
+LHBz = Drv(metric, LHSb, 'z', 1, 1);
+Qb = OMEGAX.*LHBx + OMEGAY.*LHBy + OMEGAZ.*LHBz;
+Qdir = Qmom + Qb;
+
 
 % Calculate J Vectors
 % disp('Calculate J Vectors');
@@ -60,9 +72,9 @@ V = GetVar(statefile, diagfile, {'VVEL', '(1)'}, slice);
 W = GetVar(statefile, diagfile, {'WVEL', '(1)'}, slice);
 % 
 % %ADVECTIVE TERMS
-JADVx = U.*Q; 
-JADVz = W.*Q;
-JADVy = V.*Q;
+JADVx = U.*Qdir; 
+JADVz = W.*Qdir;
+JADVy = V.*Qdir;
 %FRICTION TERMS
 Fx = GetVar(statefile, diagfile, {'TOTUTEND','Um_Advec', ' (1)/86400 - (2)'},slice);
 Fx = Fx - dpdx;
@@ -72,7 +84,7 @@ Fy = Fy - dpdy;
 JFx = -bz.*Fy; JFy = bz.*Fx; JFz = bx.*Fy - by.*Fx;
 
 %DIABATIC TERMS
-D = TtoB*GetVar(statefile, diagfile, {'TOTTTEND', 'UDIAG1', '(1)/86400 - (2)'}, slice);
+D = LHSb; %TtoB*GetVar(statefile, diagfile, {'TOTTTEND', 'UDIAG1', '(1)/86400 - (2)'}, slice);
 JBx = -OMEGAX.*D; JBy = -OMEGAY.*D; JBz = -OMEGAZ.*D;
 %%
 
